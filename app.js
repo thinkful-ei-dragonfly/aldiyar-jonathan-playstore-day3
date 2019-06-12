73ce6b387751afff4dc7a3b230ae568f10c19f75
Jonathan Moore <jmjonemoore@gmail.com>
2019-06-12 16:49:49 -0500

    Completed Assignment

--- app.js
+++ app.js
@@ -55,7 +55,6 @@ app.get('/apps', (req, res) => {
   
 })
 
+module.exports = app;
 
-app.listen(8000, () => {
-  console.log('server is on 8000');
-})
+
---
+++ test/app.test.js
@@ -0,0 +1,67 @@
+const app = require('../app');
+const expect = require('chai').expect;
+const request = require('supertest');
+
+describe('Playstore App', () => {
+  it('should return an array of apps', () => {
+    return request(app)
+    .get('/apps')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .then(res => {
+      expect(res.body).to.be.an('array');
+    })
+  })
+
+
+  it('should return a 400 when given incorrect sort param', () => {
+    return request(app)
+    .get('/apps')
+    .query({sort: 'MISTAKE'})
+    .expect(400, 'Sort must be Rating or App')
+  })
+
+
+  it('should return a 400 when given incorrect genre param', () => {
+    return request(app)
+    .get('/apps')
+    .query({genres: 'MISTAKE'})
+    .expect(400, 'Genres must be Action, Puzzle, Strategy, Casual, Arcade, or Card')
+  })
+
+  it('should sort by rating', () => {
+    return request(app)
+    .get('/apps')
+    .query({sort: 'rating'})
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .then(res => {
+      expect(res.body).to.be.an('array');
+      let i = 0;
+      let sorted = true;
+      while(sorted && i < res.body.length - 1) {
+        sorted = sorted && res.body[i].Rating <= res.body[i+1].Rating;
+        i++;
+      }
+      expect(sorted).to.be.true;
+    })
+  }) 
+
+  it('should sort by genre', () => {
+    return request(app)
+    .get('/apps')
+    .query({genres: 'Action'})
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .then(res => {
+      expect(res.body).to.be.an('array');
+      let i = 0;
+      let genres = true;
+      while(genres && i < res.body.length -1) {
+        genres = genres && res.body[i].Genres.includes('Action');
+        i++
+      }
+      expect(genres).to.be.true;
+    })
+  })
+})
